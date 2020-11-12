1a3b2ca41c210e8994e30f99ee6d7c80ce342646
Md Shahnawaz Hossain <30951203+Hossain-Shah@users.noreply.github.com>
2020-11-12 22:43:43 +0600

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,15 @@
+import requests
+from rest_framework.views import APIView
+from django.shortcuts import render
+from django.http import HttpResponse
 from django.shortcuts import render
 
+class GetPostsList(APIView):
+    def get(self, request, format=None):
+        r = requests.get("https://jsonplaceholder.typicode.com/posts")
+        return HttpResponse(r.json())
 
-# POSTS VIEW ENDPOINT
-def posts(request):
-    return render(request, 'blog-listing.html')
-
-
-# POST DETAILS VIEW ENDPOINT
-def post_details(request):
-    return render(request, 'blog-post.html')
+class GetPostDetails(APIView):
+    def get(self, request, id, format=None):
+        r = requests.get(f"https://jsonplaceholder.typicode.com/posts/{id}")
+        return HttpResponse(r.json())

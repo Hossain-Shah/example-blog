d512dfc52daeb2b97278081bf81c58ee0d9e280d
Md Shahnawaz Hossain <30951203+Hossain-Shah@users.noreply.github.com>
2020-11-12 21:37:45 +0600

    Update views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,11 @@
-from django.http import Http404
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 # LOGIN VIEW ENDPOINT
 
-def login(request):
-    return render(request, 'login.html')
-
-
-def register(request):
-    return render(request, 'register.html')
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'

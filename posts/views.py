6e0f41e54a79fb737e051657e2ac5be6d8ad889d
Md Shahnawaz Hossain <30951203+Hossain-Shah@users.noreply.github.com>
2020-11-13 17:01:56 +0600

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -6,10 +6,15 @@ from django.shortcuts import render
 
 class GetPostsList(APIView):
     def get(self, request, format=None):
-        r = requests.get("https://jsonplaceholder.typicode.com/posts")
+        r = requests.get(f"https://jsonplaceholder.typicode.com/posts")
         return HttpResponse(r.json())
 
 class GetPostDetails(APIView):
-    def get(self, request, id, format=None):
-        r = requests.get(f"https://jsonplaceholder.typicode.com/posts/{id}")
+    def get(self, request, post_id, format=None):
+        r = requests.get(f"https://jsonplaceholder.typicode.com/posts/{post_id}")
+        return HttpResponse(r.json())
+    
+class GetCommentsList(APIView):
+    def get(self, request, post_id, format=None):
+        r = requests.get(f"https://jsonplaceholder.typicode.com/posts/{post_id}/comments")
         return HttpResponse(r.json())

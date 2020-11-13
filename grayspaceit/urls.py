68d71979530ba3c5bca775b465f13592a6d175af
Md Shahnawaz Hossain <30951203+Hossain-Shah@users.noreply.github.com>
2020-11-13 21:37:53 +0600

    Update urls.py

--- grayspaceit/urls.py
+++ grayspaceit/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 from posts.views import posts
 from django.views.generic.base import TemplateView,
 from posts.views import GetPostsList
@@ -27,5 +25,5 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('posts/', include('posts.urls')),
-    path('', GetPostsList.as_view()),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', GetPostsList.as_view())
+] 

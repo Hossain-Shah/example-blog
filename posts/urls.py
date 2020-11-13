a3ca4b60dc1788225b004628b886103451747de6
Md Shahnawaz Hossain <30951203+Hossain-Shah@users.noreply.github.com>
2020-11-13 17:04:22 +0600

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from posts.views import GetPostsList, GetPostDetails
+from posts.views import GetPostsList, GetPostDetails, GetCommentsList
 
 urlpatterns = [
     path('', GetPostsList.as_view()),
-    path('<int:id>', GetPostDetails.as_view())
+    path('<int:post_id>', GetPostDetails.as_view()),
+    path('<int:post_id>/comments', GetCommentsList.as_view())
 ]

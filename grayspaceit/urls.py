ea7644179d8a639104bee01e0035efd6b7dcd5cf
Md Shahnawaz Hossain <30951203+Hossain-Shah@users.noreply.github.com>
2020-11-13 17:06:37 +0600

    Update urls.py

--- grayspaceit/urls.py
+++ grayspaceit/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home')
     path('posts/', include('posts.urls')),
-    path('', GetPostsList.as_view())
+    path('', GetPostsList.as_view()),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
